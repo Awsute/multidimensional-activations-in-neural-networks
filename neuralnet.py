d371d76fbe30730f224b6aef90759f24edc1c8ad
Awsute <63113841+Awsute@users.noreply.github.com>
2023-06-25 18:18:30 -0400

    i think i got forward propagation figured out

--- neuralnet.py
+++ neuralnet.py
@@ -1,20 +1,61 @@
 import numpy as np
 
 class Model:
-    input_layer = None
-    hidden_layers = [None]
-    def __new__(self):
-        return self
+    layers = [None]
+    def __init__(self, layers):
+        self.layers = layers
+    
     def forward(self, inputs):
-        return
+        prev_outputs = inputs
+        for i in range(len(self.layers)):
+            prev_outputs = self.layers[i].forward(prev_outputs)
+
+            if i+1 < len(self.layers):
+                intermediate_list = [[0]*len(self.layers[i].nodes)]*len(self.layers[i+1].nodes)
+                
+                for o in range(len(prev_outputs)):
+                    for j in range(len(prev_outputs[o])):
+                        intermediate_list[j][o] = prev_outputs[o][j]
+                
+                prev_outputs = intermediate_list
+                print(intermediate_list)
+
+        return prev_outputs
 
 class Layer:
-    def __new__(self):
-        return self
-class Node:
-    activation = lambda x: np.array([])
-    def __new__(self):
-        return self
-    def forward(self, inputs):
-        return self.activation(inputs)
+    nodes = [None]
+    def __init__(self, nodes):
+        self.nodes = nodes
 
+    def forward(self, input):
+        outputs = []
+        for i in range(len(self.nodes)):
+            outputs.append(self.nodes[i].forward(input[i]))
+        return outputs
+
+class Node:
+    #each node has an input list and a coefficient list
+    #output of node is a list
+    activation = lambda x, a: np.array([])
+    coef = [1]
+    def __init__(self, activation, coef):
+        self.activation = activation
+        self.coef = coef
+    
+    def forward(self, input):
+        return self.activation(input, self.coef)
+
+
+model = Model([
+    Layer(
+        [Node(lambda x, a: [a[0]*x[0], a[0]-x[0]], [0.5])]
+    ),
+        Layer(
+        [Node(lambda x, a: [a[0]*x[0], x[0]], [0.1]), Node(lambda x, a: [a[0]*x[0],x[0]], [-0.1])]
+    ),
+    Layer(
+        [Node(lambda x, a: [a[0]*x[0]+x[1]], [0.1]), Node(lambda x, a: [a[0]*x[0]+x[1]], [-0.1])]
+    )
+])
+
+print(model.forward([[0.63]]))
